463c7fe64f64ab6e39f222214321c2118c3e6802
John Hu <im@john.hu>
2017-03-08 22:17:55 +0800

    create the skeleton of ui

---
+++ ui/README.md
@@ -0,0 +1,3 @@
+# oclGA UI
+
+This is the UI for oclGA. It is created with React.js and React Bootstrap.
---
+++ ui/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="app-header">
+          <h2>oclGA UI</h2>
+        </div>
+        <div className="app-main">
+          ...
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
