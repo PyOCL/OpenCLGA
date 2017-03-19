07933c651a5b5567f08b99039dd3daaef2047083
Kilik Kuo <kikuo@mozilla.com>
2017-03-19 20:04:36 +0800

    Support python package script.

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+package_files_paths = []
+def package_files(directory):
+    global package_files_paths
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            if filename == '.gitignore':
+                continue
+            print(filename)
+            package_files_paths.append(os.path.join('..', path, filename))
+
+package_files('OpenCLGA/ui')
+package_files('OpenCLGA/kernel')
+
+setup(name='OpenCLGA',
+      version='0.1',
+      description='Run a general purpose genetic algorithm on top of pyopencl.',
+      url='https://github.com/PyOCL/oclGA.git',
+      author='John Hu(胡訓誠), Kilik Kuo(郭彥廷)',
+      author_email='im@john.hu, kilik.kuo@gmail.com',
+      license='MIT',
+      include_package_data=True,
+      packages=find_packages(),
+      package_data={
+        'OpenCLGA': package_files_paths,
+        'OpenCLGA.examples.taiwan_travel': ['kernel/*.c', '*.json'],
+      },
+      install_requires=[
+          'pycparser',
+          'cffi',
+          'numpy',
+          'wheel',
+      ],
+      zip_safe=False)
