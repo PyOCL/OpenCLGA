dc563a56505b82a72c0e66e3939dedd0283c6667
John Hu <im@john.hu>
2017-04-13 16:31:00 +0800

    Fixed #32 - move the source of demo UI to OpenCLGA-UI repo

--- OpenCLGA/ui/src/shared/utils.js
+++
@@ -1,5 +0,0 @@
-import numeral from 'numeral';
-
-export const createSimpleAction = (type) => (data) => ({ type, data });
-export const formatFitness = (value) => numeral(value).format('0,0.0000');
-export const formatGeneration = (value) => numeral(value).format('0,0');
